ad4544c8e7639124a135b7765d2d5739d4418a10
Jered Philippon <jered.philippon@wexinc.com>
2018-04-16 19:42:10 -0400

    This is crazy google test added

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -1,14 +1,49 @@
 package stepDefinitions;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import global.SharedWebDriver;
+import org.hamcrest.CoreMatchers;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Steps {
+    private static final By GOOGLE_SEARCH_BOX_ID = By.id("lst-ib");
+    private WebDriver driver = SharedWebDriver.getDriver();
 
-    @Given("^everything is working$")
-    public void everything_is_working() throws Throwable {
-        WebDriver driver = SharedWebDriver.getDriver();
-        driver.get("https://google.com");
+    @Given("^I navigate to (.*)$")
+    public void navigateTo(String website) throws Throwable {
+        driver.get(website);
+    }
+
+    @When("^I search for (.*)$")
+    public void search(String searchText) throws Throwable {
+        WebElement textBox = driver.findElement(GOOGLE_SEARCH_BOX_ID);
+        textBox.sendKeys(searchText);
+        textBox.submit();
+    }
+
+    @Then("^I should see a link for (.*)")
+    public void findOnPage(String link) {
+        WebElement element = driver.findElement(By.cssSelector("a[href*='" + link + "']"));
+        assertThat(element, CoreMatchers.notNullValue());
+    }
+
+    @When("^I click the link (.*)$")
+    public void clickLinkFor(String link) throws Throwable {
+        WebElement element = driver.findElement(By.cssSelector("a[href*='" + link + "']"));
+        element.click();
+    }
+
+    @Then("^I listen to some sweet tunes for (\\d+) minutes$")
+    public void waitForMinutes(int minutes) throws Throwable {
+        int time = minutes * 1000 * 60;
+        Thread.sleep(time);
     }
 }
--- src/test/resources/features/test1.feature
+++ src/test/resources/features/test1.feature
@@ -2,4 +2,8 @@ Feature: Gmail Login
   As a user I should able to login into Gmail.
 
   Scenario: I login with valid credential
-    Given everything is working
+    Given I navigate to https://google.com
+    When I search for This is crazy
+    Then I should see a link for https://www.youtube.com/watch?v=fWNaR-rxAic
+    When I click the link https://www.youtube.com/watch?v=fWNaR-rxAic
+    Then I listen to some sweet tunes for 1 minutes
