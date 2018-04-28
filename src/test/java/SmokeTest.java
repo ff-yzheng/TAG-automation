e1eb94042cd5970237622cd950687d4594b84227
Yingqiao Zheng <yingqiao.zheng@wexinc.com>
2018-04-28 15:54:30 -0400

    This is the same file with RunCukeTest.java at this moment, tring to name the runner file with meaningful  name

---
+++ src/test/java/SmokeTest.java
@@ -0,0 +1,18 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+//@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"global", "stepDefinitions", "pages"},
+        format = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+                "rerun:target/cucumber-reports/rerun.txt"
+
+        }
+        //pretty:target/cucumber-json-report.json
+)
+public class SmokeTest extends AbstractTestNGCucumberTests{
+}
