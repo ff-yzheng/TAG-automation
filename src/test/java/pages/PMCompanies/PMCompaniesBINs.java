2ef1b03411f3341d0ad6d175ad5529c934b646d8
Todd Gagel <todd.gagel@wexinc.com>
2018-05-14 16:45:40 -0400

    TG - Added PM Companies BINs Page Model - Added PM Companies Credit Limit Page Model

---
+++ src/test/java/pages/PMCompanies/PMCompaniesBINs.java
@@ -0,0 +1,51 @@
+package pages.PMCompanies;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import pages.TransactGlobalPage;
+
+public class PMCompaniesBINs extends TransactGlobalPage {
+
+    public PMCompaniesBINs(WebDriver driver){
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    // Page Info
+    private static final String MFA_XPATH = "//ol[@class='breadcrumb']/*[contains(text(),'BINs')]"; // An element that only shows on this page
+
+    // Corporate Account
+    @FindBy(how = How.XPATH, using = "//label[contains(text(),'Corporate Account')]/../div/span")
+    public WebElement CorporateAccount;
+
+    // Scheme
+    @FindBy(how = How.XPATH, using = "//label[contains(text(),'Scheme')]/../div/span")
+    public WebElement Scheme;
+
+    // Scheme Dropdown
+    @FindBy(how = How.XPATH, using = "//select[@name='Scheme']")
+    public WebElement SchemeDropdown;
+
+    // Currency
+    @FindBy(how = How.XPATH, using = "//label[contains(text(),'Currency')]/../div/span")
+    public WebElement Currency;
+
+    // Currency Dropdown
+    @FindBy(how = How.XPATH, using = "//select[@name='Currency']")
+    public WebElement CurrencyDropdown;
+
+    // BIN
+    @FindBy(how = How.XPATH, using = "//label[contains(text(),'BIN')]/../div/span")
+    public WebElement Bin;
+
+    // BIN Dropdown
+    @FindBy(how = How.XPATH, using = "//select[@name='bin']")
+    public WebElement BinDropdown;
+
+    // BIN Status
+    @FindBy(how = How.XPATH, using = "//label[contains(text(),'BIN Status')]/../div/span")
+    public WebElement BinStatus;
+}
---
+++ src/test/java/pages/PMCompanies/PMCompaniesCreditLimit.java
@@ -0,0 +1,47 @@
+package pages.PMCompanies;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import pages.TransactGlobalPage;
+
+public class PMCompaniesCreditLimit extends TransactGlobalPage {
+
+    public PMCompaniesCreditLimit(WebDriver driver){
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    // Page Info
+    private static final String MFA_XPATH = "//ol[@class='breadcrumb']/*[contains(text(),'Credit Limit')]"; // An element that only shows on this page
+
+    // Company Credit Limit
+    @FindBy(how = How.XPATH, using = "//input[@name='CreditLimit.Cents']")
+    public WebElement CompanyCreditLimit;
+
+    // Currency
+    @FindBy(how = How.XPATH, using = "//label[contains(text(),'Currency')]/../div/span")
+    public WebElement Currency;
+
+    // Available Credit
+    @FindBy(how = How.XPATH, using = "//label[contains(text(),'Available Credit')]/../div/span")
+    public WebElement AvailableCredit;
+
+    // Balance
+    @FindBy(how = How.XPATH, using = "//label[contains(text(),'Balance')]/../div/span")
+    public WebElement Balance;
+
+    // CL Override, Decline Authorizations radio button
+    @FindBy(how = How.XPATH, using = "//label[contains(text(),'Decline Authorizations')]/input[@name='CreditLimit.Override']")
+    public WebElement DeclineAuthsRadioButton;
+
+    // CL Override, Override Company Credit Limit radio button
+    @FindBy(how = How.XPATH, using = "//label[contains(text(),'Override')]/input[@name='CreditLimit.Override']")
+    public WebElement OverrideCCLRadioButton;
+
+    // Days to Hold Auths
+    @FindBy(how = How.XPATH, using = "//input[@name='DaysToHold']")
+    public WebElement DaysToHoldAuths;
+}
